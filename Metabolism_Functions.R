82b5a308704c5f734eb28c489a6457e80a512c9f
nrlottig <nrlottig@wisc.edu>
2018-07-12 14:57:21 -0500

    added identify pch function

--- Metabolism_Functions.R
+++ Metabolism_Functions.R
@@ -207,3 +207,18 @@ extract.metab.data <- function(dataIn, time.freq){
   return(dataIn)
 }
 
+#function to remove points 
+identifyPch <- function(x, y = NULL, n = 2, pch = 19, ...)
+{
+  xy <- xy.coords(x, y); x <- xy$x; y <- xy$y
+  sel <- rep(FALSE, length(x)); res <- integer(0)
+  while(sum(sel) < n) {
+    ans <- identify(x[!sel], y[!sel], n = 1, plot = FALSE, ...)
+    if(!length(ans)) break
+    ans <- which(!sel)[ans]
+    points(x[ans], y[ans], pch = pch)
+    sel[ans] <- TRUE
+    res <- c(res, ans)
+  }
+  res
+}
